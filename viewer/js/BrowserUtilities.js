d71cfd6421ded39c76a4bf6d4294de5545cc0c64
Monica Powell <monica@aboutmonica.com>
2020-06-24 16:31:38 -0400

    update imports to be functional

--- viewer/js/BrowserUtilities.js
+++ viewer/js/BrowserUtilities.js
@@ -1,13 +1,18 @@
 /** Returns the current width of the document. */
 export function getWidth() {
-    return document.documentElement.clientWidth;
+  return document.documentElement.clientWidth;
 }
 /** Returns the current height of the document. */
 export function getHeight() {
-    return document.documentElement.clientHeight;
+  return document.documentElement.clientHeight;
 }
 /** Returns true if the browser is zoomed in with pinch-to-zoom on mobile. */
 export function isZoomed() {
-    return (getWidth() !== window.innerWidth);
+  return getWidth() !== window.innerWidth;
 }
+export default {
+  getWidth: getWidth,
+  getHeight: getHeight,
+  isZoomed: isZoomed,
+};
 //# sourceMappingURL=BrowserUtilities.js.map
