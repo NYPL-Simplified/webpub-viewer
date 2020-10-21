70dfb50a1aa2764f92015da0ece9a4f112c2e266
Crystal Ngai <hokei@users.noreply.github.com>
2020-10-21 17:33:31 -0400

    Added a different tsconfig for linting

--- src/sw.ts
+++ src/sw.ts
@@ -1,21 +1,25 @@
 const CACHE_NAME = "webpub-viewer";
 
-self.addEventListener('activate', () => {
-    (self as any).clients.claim();
+self.addEventListener("activate", () => {
+  (self as any).clients.claim();
 });
 
-self.addEventListener('fetch', event => {
-    // Response from the cache immediately if possible, but also fetch an update.
+self.addEventListener("fetch", event => {
+  // Response from the cache immediately if possible, but also fetch an update.
 
-    const cachedOrFetchedResponse = self.caches.open(CACHE_NAME).then((cache: any) => {
-        return self.caches.match((event as any).request).then(cacheResponse => {
-            const fetchPromise: Promise<any> = self.fetch((event as any).request).then((fetchResponse) => {
-                cache.put((event as any).request, fetchResponse.clone());
-                return fetchResponse;
-            });
-            return cacheResponse || fetchPromise;
-        });
+  const cachedOrFetchedResponse = self.caches
+    .open(CACHE_NAME)
+    .then((cache: any) => {
+      return self.caches.match((event as any).request).then(cacheResponse => {
+        const fetchPromise: Promise<any> = self
+          .fetch((event as any).request)
+          .then(fetchResponse => {
+            cache.put((event as any).request, fetchResponse.clone());
+            return fetchResponse;
+          });
+        return cacheResponse || fetchPromise;
+      });
     });
 
-    (event as any).respondWith(cachedOrFetchedResponse);
+  (event as any).respondWith(cachedOrFetchedResponse);
 });
